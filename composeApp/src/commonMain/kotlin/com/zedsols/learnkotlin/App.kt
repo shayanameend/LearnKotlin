cc58ed88a164c8bb1b12bf4dbe1333bb293e63a1
Shayan Ameen <shayan.ameen.developer@gmail.com>
2025-05-24 01:38:51 +0500

    Removed unused `Preview` import and reordered existing imports.

--- composeApp/src/commonMain/kotlin/com/zedsols/learnkotlin/App.kt
+++ composeApp/src/commonMain/kotlin/com/zedsols/learnkotlin/App.kt
@@ -12,11 +12,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import org.jetbrains.compose.resources.painterResource
-import org.jetbrains.compose.ui.tooling.preview.Preview
-
 import learnkotlin.composeapp.generated.resources.Res
 import learnkotlin.composeapp.generated.resources.compose_multiplatform
+import org.jetbrains.compose.resources.painterResource
 
 @Composable
 fun App() {
